d443b4dbc66b4df6ca5e1cdf6f3e2f331eed0184
Hernani Fernandes <hernanif1@gmail.com>
2021-12-26 20:07:43 +0100

    update e2e ci

--- frontend/cypress/integration/users.spec.ts
+++ frontend/cypress/integration/users.spec.ts
@@ -1,9 +1,11 @@
 context('App', () => {
   beforeEach(() => {
+    cy.intercept(`${Cypress.env('API_URL')}/accounts`, [])
     cy.visit('/users')
   })
 
   it('should render text', () => {
     cy.contains('Users')
+    cy.contains('No users to display')
   })
 })
